aff5b3ea6c36adb93a13cd2ac951c72963a8fc65
TabsVsSpaces <>
2019-06-02 04:04:59 +0200

    enhanced JDBC ConnPooling

--- Dashboard/src/Helper/DataManager.java
+++ Dashboard/src/Helper/DataManager.java
@@ -11,15 +11,15 @@ import java.util.Properties;
 
 public class DataManager {
 
-    public static Properties getPropFile(String PropertiesFile) {
+    public static Properties getPropFile(String PropertyName) {
         
         Properties props = new Properties();
         try {
-            InputStream input = DataManager.class.getClassLoader().getResourceAsStream(PropertiesFile);
+            InputStream input = DataManager.class.getClassLoader().getResourceAsStream(PropertyName);
             props.load(input);
             input.close();
         } catch (FileNotFoundException ex) {
-            LogHandler.add(PropertiesFile + " nicht gefunden!");
+            LogHandler.add(PropertyName + " nicht gefunden!");
         } catch (IOException ex) {
             LogHandler.add(ex.getMessage());
         }
--- Dashboard/src/Model/DataSource.java
+++ Dashboard/src/Model/DataSource.java
@@ -12,22 +12,22 @@ import org.apache.commons.dbcp2.BasicDataSource;
 
 public class DataSource {
 
-    private static final String PROP_FILE = "DBconnection.properties";
+    private static final String PROP_NAME = "DBconnection.properties";
 
     private static DataSource ds;
     private BasicDataSource basicDS = new BasicDataSource();
 
     private DataSource() {
 
-        basicDS.setDriverClassName(DataManager.getPropFile(PROP_FILE).getProperty("JDBC_DRIVER"));
-        basicDS.setUsername(DataManager.getPropFile(PROP_FILE).getProperty("USER"));
-        basicDS.setPassword(DataManager.getPropFile(PROP_FILE).getProperty("PASS"));
-        basicDS.setUrl(DataManager.getPropFile(PROP_FILE).getProperty("DB_URL"));
+        basicDS.setDriverClassName(DataManager.getPropFile(PROP_NAME).getProperty("JDBC_DRIVER"));
+        basicDS.setUsername(DataManager.getPropFile(PROP_NAME).getProperty("USER"));
+        basicDS.setPassword(DataManager.getPropFile(PROP_NAME).getProperty("PASS"));
+        basicDS.setUrl(DataManager.getPropFile(PROP_NAME).getProperty("DB_URL"));
 
-        basicDS.setInitialSize(Integer.parseInt(DataManager.getPropFile(PROP_FILE).getProperty("initialSize")));
-        basicDS.setMaxTotal(Integer.parseInt(DataManager.getPropFile(PROP_FILE).getProperty("maxSize")));
-        basicDS.setMinIdle(Integer.parseInt(DataManager.getPropFile(PROP_FILE).getProperty("minIdle")));
-        basicDS.setMaxIdle(Integer.parseInt(DataManager.getPropFile(PROP_FILE).getProperty("maxIdle")));
+        basicDS.setInitialSize(Integer.parseInt(DataManager.getPropFile(PROP_NAME).getProperty("initialSize")));
+        basicDS.setMaxTotal(Integer.parseInt(DataManager.getPropFile(PROP_NAME).getProperty("maxSize")));
+        basicDS.setMinIdle(Integer.parseInt(DataManager.getPropFile(PROP_NAME).getProperty("minIdle")));
+        basicDS.setMaxIdle(Integer.parseInt(DataManager.getPropFile(PROP_NAME).getProperty("maxIdle")));
 
     }
 
