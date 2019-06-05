f0ab4fd51b9b68cceaff799a10ef1ca7853ae88a
Tom <Tom@DESKTOP-JN3SB55.fritz.box>
2019-06-05 12:22:25 +0200

    Logger Added

--- Dashboard/src/DatabaseViewController.java
+++ Dashboard/src/DatabaseViewController.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+import Helper.LogHandler;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import Model.*;
 
 /**
  * FXML Controller class
@@ -19,13 +18,80 @@ public class DatabaseViewController implements Initializable {
 
     @FXML
     private AnchorPane DatabaseView;
+    @FXML
+    private TextField user;
+    @FXML
+    private TextField dbURL;
+    @FXML
+    private TextField password;
+    @FXML
+    private TextField jdbcPort;
 
     /**
      * Initializes the controller class.
      */
+
+   // TextField[] field = {dbURL,jdbcPort,user,password };
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
+    
+    private String[] getTextfield(){
+    
+        String[] connection = new String[4];
+        
+        connection[0] = jdbcPort.getText();
+        connection[1] = dbURL.getText();
+        connection[2] = user.getText();
+        connection[3] = password.getText();
+       
+        /*
+        for(int i = 0; i<4; i++)
+        {
+            connection[i] = field[i].getText();
+         if ((field[i].getText() != null && !field[i].getText().isEmpty())) {
+           connection[i] = field[i].getText();
+            } 
+         else {
+            return null;
+            
+        }
+       } 
+        */
+       
+        return connection;
+    }
+
+    private void clearText(){
+        
+        dbURL.clear();
+        jdbcPort.clear();
+        user.clear();
+        password.clear();
+    }
+    
+    @FXML
+    private void testConnection(MouseEvent event)throws Exception {
+
+        String[] connection = getTextfield();
+
+        
+        if ( !connection[0].isEmpty() && !connection[1].isEmpty() && !connection[2].isEmpty() && !connection[3].isEmpty())
+        {
+            JDBCTest.getState(connection[0], connection[1], connection[2], connection[3]);
+        }
+        else {
+           LogHandler.add("Bitte alle Fenster ausfüllen");
+        }
+    }
+
+    @FXML
+    private void setConnection(MouseEvent event) {
+        String[] connection = getTextfield();
+        // set Database conn and save (jdbc-pool)
+        
+    }
     
 }
--- Dashboard/src/MainController.java
+++ Dashboard/src/MainController.java
@@ -13,6 +13,15 @@ import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import Helper.*;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.scene.control.ListView;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 
 /**
  * FXML Controller class
@@ -26,12 +35,33 @@ public class MainController implements Initializable {
     @FXML
     private Pane PaneView;
 
+    protected ListProperty<String> listProperty = new SimpleListProperty<>();
+    
+    @FXML
+    private ListView myLog;
+    
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    public void initialize(URL url, ResourceBundle rb)  {
+       
+      myLog.itemsProperty().bind(listProperty);
+      LogHandler.add("Clicked");
+      listProperty.set(FXCollections.observableArrayList(LogHandler.show()));
+      
+      Runnable runnable = new Runnable() {
+      public void run() {
+          
+     // myLog.itemsProperty().bind(listProperty);
+      listProperty.set(FXCollections.observableArrayList(LogHandler.show()));
+      }
+    };
+    
+    ScheduledExecutorService service = Executors
+                    .newSingleThreadScheduledExecutor();
+    service.scheduleAtFixedRate(runnable, 0, 5, TimeUnit.SECONDS);
+
     }    
 
     @FXML
@@ -40,7 +70,7 @@ public class MainController implements Initializable {
 
     @FXML
     private void changeReport(MouseEvent event) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("ChangeReport.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("ChangeReport.fxml"));  
         PaneView.getChildren().addAll(root);
     }
 
@@ -55,5 +85,9 @@ public class MainController implements Initializable {
         Parent root = FXMLLoader.load(getClass().getResource("DatabaseView.fxml"));
         PaneView.getChildren().addAll(root);
     }
-    
+
+
 }
+
+    
+
