79fe5036c863a7ffa4b4b2608300530a143826da
TabsVsSpaces <>
2019-06-07 21:26:08 +0200

    SQLHandler returns HashMap

--- Dashboard/src/Helper/LogHandler.java
+++ Dashboard/src/Helper/LogHandler.java
@@ -1,40 +1,39 @@
-
+/*
+TODO
+    -unification of own written Exceptions and SQLExceptions (format)
+    - Erklärung Liste: https://docs.oracle.com/javase/1.5.0/docs/api/java/util/ArrayList.html ; http://www.codeadventurer.de/?p=1751
+ */
 package Helper;
 
 import java.util.*;
 import java.text.*;
 
-/*
-TODO: 
-    - Erklärung Liste: https://docs.oracle.com/javase/1.5.0/docs/api/java/util/ArrayList.html ; http://www.codeadventurer.de/?p=1751
- */
-
 public class LogHandler {
+
     private static final String PROP_NAME = "Application.properties";
     private static final int LOG_SIZE = Integer.parseInt(DataManager.getProperties(PROP_NAME).getProperty("LogSize"));
-    
+
     //ArrayList for dynamic add of strings
     private static final ArrayList<String> ERROR_LOG = new ArrayList<>();
-    
-    
+
     public static void add(String Fehler) {
-        ERROR_LOG.add(getCurrDate()+"    "+Fehler);
-        
+        ERROR_LOG.add(getCurrDate() + "    " + Fehler);
+
         //prune the list to n elements (n=LOG_SIZE)     
-        if(ERROR_LOG.size()>LOG_SIZE){
-            while(ERROR_LOG.size()>LOG_SIZE){
+        if (ERROR_LOG.size() > LOG_SIZE) {
+            while (ERROR_LOG.size() > LOG_SIZE) {
                 ERROR_LOG.remove(0);
             }
         }
     }
-    
-    public static String[] show(){
-        String LogFenster [] = new String [ERROR_LOG.size()];
+
+    public static String[] show() {
+        String LogFenster[] = new String[ERROR_LOG.size()];
         LogFenster = ERROR_LOG.toArray(LogFenster);
         return LogFenster;
     }
-    
-    public static String getCurrDate(){
+
+    public static String getCurrDate() {
         Date dNow = new Date();
         SimpleDateFormat simpleForm = new SimpleDateFormat("yyyy.MM.dd hh:mm:ss");
         return simpleForm.format(dNow);
--- Dashboard/src/Model/SQLHandler.java
+++ Dashboard/src/Model/SQLHandler.java
@@ -1,46 +1,61 @@
 /*
 TODO
-    -define output!
-    -examineResultSet() is missing!
+    -setter for map is mising
+    -examineResultSet() is missing! ->extra class mit new SQLHandler and getMap()
+    -possible solution for saving the resultSet http://commons.apache.org/proper/commons-beanutils/apidocs/org/apache/commons/beanutils/RowSetDynaClass.html
  */
 package Model;
 
 import Helper.*;
+import java.util.*;
 import java.sql.*;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 public class SQLHandler {
 
+    private static ResultSet rs = null;
+    public static Map<String, List<Object>> map;
+
     public void queryStatement(String sqlStatement) {
-        SQLHandler sqlHandler = new SQLHandler();
-        try {
-            sqlHandler.handleStatement(sqlStatement);
-        } catch (Exception e) {
-            LogHandler.add(e.getMessage());
+
+        SQLHandler sqlHandler = new SQLHandler();;
+
+        sqlHandler.getResultSet(sqlStatement);
+        if (map != null) {
+            try {
+                Iterator<Map.Entry<String, List<Object>>> entries = map.entrySet().iterator();
+                while (entries.hasNext()) {
+                    Map.Entry<String, List<Object>> entry = entries.next();
+                    System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+                }
+                System.out.println("Erfolg");
+            } catch (Exception se) {
+                LogHandler.add(se.getMessage());
+            }
+        } else {
+            System.out.println("Fehler");
         }
     }
-    
+
     //Method to handle SQLExceptions primarily //returns an resultSet
-    private void handleStatement(String sqlStatement) {
+    private void getResultSet(String sqlStatement) {
 
         Connection conn = null;
         Statement stmt = null;
+
         try {
             BasicDataSource basicDS = JDBCPool.getInstance().getBasicDS();
             conn = basicDS.getConnection();
             stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sqlStatement);
-            /*
-            while(rs.next()) {
-            ...
-            alternat.return rs?
-            }
-             */
-            rs.close();
+            rs = stmt.executeQuery(sqlStatement);
+            resultSetToArrayList(rs);
         } catch (SQLException se) {
             LogHandler.add(se.getMessage());
         } finally {
             try {
+                if (rs != null) {
+                    rs.close();
+                }
                 if (stmt != null) {
                     stmt.close();
                 }
@@ -51,5 +66,21 @@ public class SQLHandler {
                 LogHandler.add(se.getMessage());
             }
         }
+    }
+
+    //converts the resultSet to a map of list
+    private Map<String, List<Object>> resultSetToArrayList(ResultSet rs) throws SQLException {
+        ResultSetMetaData md = rs.getMetaData();
+        int columns = md.getColumnCount();
+        map = new HashMap<>(columns);
+        for (int i = 1; i <= columns; ++i) {
+            map.put(md.getColumnName(i), new ArrayList<>());
+        }
+        while (rs.next()) {
+            for (int i = 1; i <= columns; ++i) {
+                map.get(md.getColumnName(i)).add(rs.getObject(i));
+            }
+        }
+        return map;
     }
 }
